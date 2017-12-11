3c3e3c5ab73c95758c21c881f24f742b506a2ecf
夜&猫子 <tql163.love@163.com>
2017-12-11 15:40:17 +0800

    1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ var argv = require('minimist')(process.argv.slice(2), {
         return '(' + argv.mod.replace(/,/g, '|') + ')';
       }() : '',
       src =[    
-        //'./node_modules/jquery/dist/jquery.js',
+        // './node_modules/jquery/dist/jquery.js',
         './node_modules/html2canvas/dist/html2canvas.js',
         './node_modules/webuploader/dist/webuploader.html5only.js',
         './src/**/viewEdit.js',
@@ -79,7 +79,7 @@ var argv = require('minimist')(process.argv.slice(2), {
   alljs: function(ver) {
     ver = ver === 'open';
     var src = [    
-        //'./node_modules/jquery/dist/jquery.js',
+        // './node_modules/jquery/dist/jquery.js',
         './node_modules/html2canvas/dist/html2canvas.js',        
         './node_modules/webuploader/dist/webuploader.html5only.js',
         './src/**/viewEdit.js',
